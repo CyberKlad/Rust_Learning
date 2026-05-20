fbfdbf4c59e49835db34139885c2656f29a59f0e
CyberKlad <122490236+CyberKlad@users.noreply.github.com>
2026-05-20 10:16:22 -0700

    adding temp converter
    this was the first program written while learning rust

---
+++ temperature_converter/target/debug/deps/temperature_converter-9a0a79f5bebc7692.d
@@ -0,0 +1,5 @@
+/Users/korbingillette/RustProjects/temperature_converter/target/debug/deps/temperature_converter-9a0a79f5bebc7692.d: src/main.rs
+
+/Users/korbingillette/RustProjects/temperature_converter/target/debug/deps/temperature_converter-9a0a79f5bebc7692: src/main.rs
+
+src/main.rs:
