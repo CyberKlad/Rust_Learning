13e3b345e1689fd45390583abdf38c3670d34e65
CyberKlad <122490236+CyberKlad@users.noreply.github.com>
2026-05-20 10:21:28 -0700

    starting cli app
    this will be my first rust cli app that uses IO

---
+++ minigrep/target/debug/deps/minigrep-7622f549dc6c8425.d
@@ -0,0 +1,5 @@
+/Users/korbingillette/RustProjects/Rust_Learning/minigrep/target/debug/deps/minigrep-7622f549dc6c8425.d: src/main.rs
+
+/Users/korbingillette/RustProjects/Rust_Learning/minigrep/target/debug/deps/minigrep-7622f549dc6c8425: src/main.rs
+
+src/main.rs:
