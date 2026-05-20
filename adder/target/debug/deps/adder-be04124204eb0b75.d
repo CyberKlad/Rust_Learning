ad1273e9dfe8246b59644480c8930faaa0b63d29
CyberKlad <122490236+CyberKlad@users.noreply.github.com>
2026-05-20 10:09:55 -0700

    Create adder
    This is a file containing unit and integration test

---
+++ adder/target/debug/deps/adder-be04124204eb0b75.d
@@ -0,0 +1,7 @@
+/Users/korbingillette/RustProjects/adder/target/debug/deps/adder-be04124204eb0b75.d: src/lib.rs
+
+/Users/korbingillette/RustProjects/adder/target/debug/deps/libadder-be04124204eb0b75.rlib: src/lib.rs
+
+/Users/korbingillette/RustProjects/adder/target/debug/deps/libadder-be04124204eb0b75.rmeta: src/lib.rs
+
+src/lib.rs:
---
+++ adder/target/debug/deps/integration_test-6ae3b9ece9b0bf98.d
@@ -0,0 +1,6 @@
+/Users/korbingillette/RustProjects/adder/target/debug/deps/integration_test-6ae3b9ece9b0bf98.d: tests/integration_test.rs tests/common/mod.rs
+
+/Users/korbingillette/RustProjects/adder/target/debug/deps/integration_test-6ae3b9ece9b0bf98: tests/integration_test.rs tests/common/mod.rs
+
+tests/integration_test.rs:
+tests/common/mod.rs:
